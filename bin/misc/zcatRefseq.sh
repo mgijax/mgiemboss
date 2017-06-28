ec63c151a91f66bb898710c8a7d93fd11019163f
LucyHut <lucie.hutchins@yahoo.com>
2017-06-28 13:44:06 -0400

    Code refactoring

--- bin/misc/zcatRefseq.sh
+++ bin/misc/zcatRefseq.sh
@@ -1,65 +1,59 @@
 #!/bin/sh
 # Version 2 12/7/2009
-# Version 3 01/23/2011
-# sc - updated 9/5/2013
-# sc - update 09/10/2013
+# sc - updated 01/07/2013
+# sc - updated 09/10/2013
 #
-# lnh - updated 1/29/2015
+# lnh - updated 11/05/2014
 #
-# This script is called from the updateRefseq.sh script
+# This script is called from the updateGenbank.sh script
 # Asumption - all the global variables have already been set
 #
 # It took about 3 hours to run
 # 
-
 datadir=$sourcedir
 destination=$temp_dir
+
 
 if [ ! -d $destination ]
 then
     echo "Creating $destination"
     mkdir  $destination
-else
-    echo "Removing temp files"
-    if ! find $destination -maxdepth 0 -empty | read
-    then
-      rm $destination/*
-    fi
 fi
 
 echo "start: " `date`
 echo "Data directory: " $datadir
 echo "Destination directory: " $destination
-echo "The file prefix: " $filePrefix
 
 cd $datadir
+echo "File Count in $datadir:"
+ls -l *.gz | wc -l
 
-for file in `ls $filePrefix*.rna.g?ff.gz |sed s/.gz//`
+for file_group in $REMOTE_FILES
 do
+   echo "File count for $file_group:"
+   ls -l *$file_group.gz | wc -l
+   echo "Removing temp files"
+   if ! find $destination -maxdepth 0 -empty | read
+   then
+      echo "rm $destination/*$file_group"
+      rm $destination/*$file_group
+   fi
+   for file in `ls -f *$file_group.gz |sed s/.gz//`
+   do
 	if [ -f $destination/$file ] 
 	then
-		echo "already expanded" $file
+		echo "already expanded:" $file
 	else
  		echo "Expanding this file: " $file
+                #set the release number if not set
 		# sc - strip out lines  beginning with 'PROJECT' and 'DBLINK'
 		$zcatprog ${file}.gz  | sed -e '/^PROJECT/d' | sed -e '/^DBLINK/d' > $destination/${file}
 	fi
-done
 
-for file in `ls $filePrefix*.protein.g?ff.gz |sed s/.gz//`
-do
-	if [ -f $destination/$file ] 
-	then
-		echo "already expanded" $file
-	else
- 		echo "Expanding this file: " $file
-	# sc - strip out lines  beginning with 'PROJECT' and 'DBLINK'
-	$zcatprog ${file}.gz  | sed -e '/^PROJECT/d' | sed -e '/^DBLINK/d' > $destination/${file}
-	fi
+    done
 done
 
 echo 'File Count:'
-ls -l $destination/$filePrefix* | wc -l
+ls -l $destination/ | wc -l
 
 echo "end: " `date`
-
