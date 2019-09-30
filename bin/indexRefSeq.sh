58a372e17916323e59ae225c0207fb58dae19ed3
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2019-09-30 11:54:02 -0400

    tr13065 removed verbose refseq configuration

--- bin/indexRefSeq.sh
+++ bin/indexRefSeq.sh
@@ -21,33 +21,29 @@ genomic.gbff
 protein.gpff"
 
 SCRIPT_NAME=`basename $0`
-LOG_DIR=$logdir
 #set this just for checking if update is needed 
 embossdb=refseqRna
 logfile=$embossdb$moddate_prefix.log
-last_updatefile=${LOG_DIR}/$logfile
+last_updatefile=$logdir/$logfile
 
-LOG=${LOG_DIR}/${SCRIPT_NAME}.log
+LOG=$logdir/${SCRIPT_NAME}.log
 rm -f ${LOG}
 touch ${LOG}
 echo "Running $0" | tee -a  ${LOG}
 
 date | tee -a ${LOG}
-sourcedir=$refseq_sourcedir
 embossdbdir=$refseq_dbbasedir
 temp_dir=$embossdbdir/temp
 
 echo "" | tee -a ${LOG}
 echo "Global variables setting" | tee -a ${LOG}
 echo "------------------------" | tee -a ${LOG}
-echo "sourcedir=$sourcedir" | tee -a ${LOG}
-echo "rna_file=$refseq_rna_file" | tee -a ${LOG}
-echo "protein_file=$refseq_protein_file" | tee -a ${LOG}
+echo "sourcedirs=$refseq_source_mammalian_dir $refseq_source_other_dir" | tee -a ${LOG}
+echo ""
 echo "zcatprog=$zcatprog" | tee -a ${LOG}
 echo "temp_dir=$temp_dir" | tee -a ${LOG}
 
-export temp_dir sourcedir zcatprog REMOTE_FILES
-export refseq_rna_file refseq_protein_file
+export embossdbdir temp_dir zcatprog REMOTE_FILES
 echo "------------------------" | tee -a ${LOG}
 echo "" | tee -a ${LOG}
 
@@ -66,7 +62,6 @@ fi
 #
 # set the release id
 #
-#release_number_file=$sourcedir/RELEASE_NUMBER
 echo "Checking if update needed " | tee -a ${LOG}
 release=`cat $refseq_release_file`
 if [ "$release" = "$last_update" ]
@@ -75,10 +70,12 @@ then
   echo "Program complete"
   exit
 fi
+
+
 echo "New release $release" |  tee -a ${LOG}
 echo "Calling " $zcatRefSeqScript | tee -a ${LOG}
-
 $zcatRefSeqScript | tee -a ${LOG}
+
 date | tee -a ${LOG}
 $update_rna  
 
@@ -89,6 +86,3 @@ echo "${SCRIPT_NAME} completed successfully " | tee -a ${LOG}
 date | tee -a ${LOG}
 
 exit 0
-
-
-
